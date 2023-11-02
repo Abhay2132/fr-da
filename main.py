a3b746686c56c90432aaae728d535e3525b8514b
root <root@localhost.localdomain>
2023-11-02 12:36:09 +0000

    search bar

--- main.py
+++ main.py
@@ -6,23 +6,31 @@ class App(ctk.CTk):
 		super().__init__()
 		self.geometry("800x400")
 		self.title("Abhay")
-		self.resizable(False, True)
+		# self.resizable(False, False)
 		
-		mainFrame = ui_.MainFrame(self)
-		leftFrame = ui_.LeftFrame(self)
-		rightFrame = ui_.RightFrame(self)
+		self.mainFrame = ui_.MainFrame(self)
+		self.leftFrame = ui_.LeftFrame(self)
+		self.rightFrame = ui_.RightFrame(self)
 		
 		self.grid_columnconfigure((0,2), minsize=150);
-		self.grid_columnconfigure(1,minsize=500);
+		self.grid_columnconfigure(1, weight=1, minsize=500);
 		self.grid_rowconfigure(0, minsize=150, weight=1);
 		
 		padx = 3
 		pady = 6
-		mainFrame.grid(padx=padx, pady=pady,row=0, column=1, sticky="nswe")
-		leftFrame.grid(row=0, padx=padx, pady=pady,column=0, sticky="nswe")
-		rightFrame.grid(row=0, padx=padx, pady=pady,column=2, sticky="nswe")
 		
+		self.mainFrame.grid(padx=padx, pady=pady,row=0, column=1, sticky="nswe")
+		self.leftFrame.grid(row=0, padx=padx, pady=pady,column=0, sticky="nswe")
+		self.rightFrame.grid(row=0, padx=padx, pady=pady,column=2, sticky="nswe")
 		
+		self.bind("<Configure>", self.on_resize)
+		
+	def on_resize(self, event):
+		screen_height = event.height
+		screen_width = event.width
+		width = screen_width - 300
+		h = 9/16*width
+		self.mainFrame.label.configure(text=("width :" +str(event.width)+"px"))
 		
 if __name__ == "__main__":
 	# ctk.set_default_color_theme("green")
--- ui_.py
+++ ui_.py
@@ -4,8 +4,8 @@ class MainFrame(ctk.CTkFrame) :
 	def __init__(self, master):
 		super().__init__(master)
 		
-		self.grid_columnconfigure(0, minsize=150, weight=1);
-		self.grid_rowconfigure(0, minsize=150, weight=1);
+		self.grid_columnconfigure(0, minsize=150, weight=1)
+		self.grid_rowconfigure(0, minsize=150, weight=1)
 		self.label = ctk.CTkLabel(master=self, text="main_frame", corner_radius=10, height=350, text_color="#ffffff", fg_color="#333333")
 		self.label.grid(pady=10, padx=5, row=0, column=0, sticky="we")
 
@@ -13,8 +13,14 @@ class LeftFrame(ctk.CTkFrame) :
 	def __init__(self, master):
 		super().__init__(master)
 		
-		self.button = ctk.CTkButton(master=self, text="left frame")
-		self.button.grid(row=0, column=0, sticky="e")
+		self.grid_columnconfigure(0, weight=1)
+		self.label = ctk.CTkLabel(master=self, text="Records")
+		self.label.grid( row=0, column=0, sticky="w", padx=10)
+		
+		ctk.CTkFrame(master=self, height=2, fg_color=("#eee", "#333")).grid(row=1, column=0, sticky="we")
+		
+		self.searchBar = SearchBar(master=self)
+		self.searchBar.grid(row=2, column=0, sticky="we")
 
 class RightFrame(ctk.CTkFrame) :
 	def __init__(self, master):
@@ -22,4 +28,19 @@ class RightFrame(ctk.CTkFrame) :
 		
 		self.button = ctk.CTkButton(master=self, text="right frame")
 		self.button.grid(row=0, column=0, sticky="e")
+		
+
+class SearchBar(ctk.CTkFrame) :
+	def __init__(self, master):
+		super().__init__(master, fg_color="transparent" )
+		
+		self.grid_columnconfigure(0, weight=1)
+		self.grid_columnconfigure(1, weight=0)
+		
+		self.entry = ctk.CTkEntry(master=self, fg_color="transparent", placeholder_text="Search User")
+		self.entry.grid(row=0, column=0, sticky="we")
+		
+		self.submit = ctk.CTkButton(master=self, text="->", width=30)
+		self.submit.grid(padx=(3,0), row=0, column=1)
+		
 		
